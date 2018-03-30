8e9881385b2b2c9a56845ca7c5e165862645c71d
hyuncheol <hyuncheol@MOON>
2018-03-30 16:49:27 +0900

    페이지 이동 버튼 추가

--- www/js/controller/dashboard/sample1.js
+++ www/js/controller/dashboard/sample1.js
@@ -1,7 +1,19 @@
 define(["app"],
     function(app) {
 	    console.log(app)
-	    app.controller("dashboardSample1Ctrl", function($scope) {
-		    $scope.message = "Message from HomeCtrl";
+	    app.controller("dashboardSample1Ctrl", function($scope, $location) {
+		    var self = $scope;
+
+		    self.title = "Sample page1";
+		    console.log($location)
+
+
+		    self.goToOption = function() {
+
+			    $location.path("/option/sample2");
+
+		    }
+
+
 	    });
     });
--- www/js/controller/option/sample2.js
+++ www/js/controller/option/sample2.js
@@ -1,7 +1,18 @@
 define(["app"],
     function(app) {
 	    console.log(app)
-	    app.controller("optionSample2Ctrl", function($scope) {
-		    $scope.message = "Message from HomeCtrl";
+	    app.controller("optionSample2Ctrl", function($scope, $location) {
+		    var self = $scope;
+		    self.title = "Sample page 2";
+
+
+		    self.goToMain = function() {
+
+			    $location.path("/dashboard/sample1");
+
+		    }
+
+
 	    });
+
     });
